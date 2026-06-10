4bcb9f3f94069fb2c4c344bf6bd8dd925e561ac0
aditya-nambiar2007 <nambiaraditya2007@gmail.com>
2026-06-10 12:52:46 +0530

    Socket.io updates

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 import { createHash } from 'crypto';
 import root from './dir.json' with { type: "json" };
+import * as answers from "./ans.json" with { type: "json" };
 
 const app = express();
 const server = createServer(app);
@@ -16,9 +17,35 @@ const io = new Server(server, {
 });
 const PORT = process.env.port || 3000;
 
+const points = {
+
+}
+
 // Socket.io connection handler
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
+
+    socket.on('submit_answer', (data) => {
+        const { questionId, answer, group } = data;
+        if (Object.hasOwn(answers, questionId) && answers[questionId].answer === answer && !answers[questionId].answered_by.includes(group)) {
+            points[group] += answers[questionId].points;
+            answers[questionId].answered_by.push(group);
+            answers[questionId].points -= 2; // Reduce points for subsequent correct answers
+            socket.emit('answer_result', { success: true, message: 'Correct answer!' });
+            io.emit('points', points);
+        }
+        else {
+            if (!Object.hasOwn(answers, questionId)) {
+                socket.emit('answer_result', { success: false, message: 'Invalid question ID!' });
+                return;
+            }
+            else if (answers[questionId].answered_by.includes(group)) {
+                socket.emit('answer_result', { success: false, message: 'Your group has already answered this question!' });
+                return;
+            }
+            else socket.emit('answer_result', { success: false, message: 'Incorrect answer . Try again!' });
+        }
+    }) 
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
@@ -29,68 +56,77 @@ io.on('connection', (socket) => {
 
 //Host frontend on the same server
 if (process.env.NODE_ENV == "production") {
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
-  })
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
 
-app.get('/api/directory', (req, res) => {
-    const dir = req.query.path?.split(/\/|\\/g).filter(Boolean) ?? [];
-     let response = root.root; 
-    for (const d of dir) {
-        if (d === "root"||d===''||d==='sub_dir'||d==='metadata') continue;
-        
-        if (!response.sub_dir || !Object.hasOwn(response.sub_dir, d)) {
-            return res.status(404).json({ error: 'Directory not found' });
+    app.get('/api/directory', (req, res) => {
+        const dir = req.query.path?.split(/\/|\\/g).filter(Boolean) ?? [];
+        let response = root.root;
+        for (const d of dir) {
+            if (d === "root" || d === '' || d === 'sub_dir' || d === 'metadata') continue;
+
+            if (!response.sub_dir || !Object.hasOwn(response.sub_dir, d)) {
+                return res.status(404).json({ error: 'Directory not found' });
+            }
+            response = response.sub_dir[d];
         }
-        response = response.sub_dir[d];
-    }
 
-    if (response.sub_dir === undefined) {
-        return res.status(400).json({ error: 'Path points to a file, not a directory' });
-    }
-
-    const output = {};
-    for (const [k, v] of Object.entries(response.sub_dir)) {
-        if (v && typeof v === 'object' && v.sub_dir !== undefined) {
-            output[k] = { metadata: { ...v.metadata, type: 'dir' } };
-        } else if (v && v.metadata && v.metadata.password !== undefined) {
-            output[k] = { content: "This file is password protected." };
-        } else {
-            output[k] = v;
+        if (response.sub_dir === undefined) {
+            return res.status(400).json({ error: 'Path points to a file, not a directory' });
         }
-    }
 
-    return res.json(output);
-});
-
-// 2. Fetch File Contents
-app.get('/api/file', (req, res) => {
-    const dir = req.query.file_path?.split(/\/|\\/g).filter(Boolean) ?? [];
-    
-    let response = root.root; 
-
-    for (const d of dir) {
-        if (d === "root"||d===''||d==='sub_dir'||d==='metadata') continue;
-        
-        if (!response.sub_dir || !Object.hasOwn(response.sub_dir, d)) {
-            return res.status(404).json({ error: 'File not found' });
+        const output = {};
+        for (const [k, v] of Object.entries(response.sub_dir)) {
+            if (v && typeof v === 'object' && v.sub_dir !== undefined) {
+                output[k] = { metadata: { ...v.metadata, type: 'dir' } };
+            } else if (v && v.metadata && v.metadata.password !== undefined) {
+                output[k] = { content: "This file is password protected." };
+            } else {
+                output[k] = v;
+            }
         }
-        response = response.sub_dir[d];
-    }
-    if (!response || response.sub_dir !== undefined) {
-        return res.status(400).json({ error: 'Path points to a directory, not a file' });
-    }
-    if (response.metadata?.password !== undefined) {
-        const providedPasswordHash = hash(req.query.password ?? '');
-        if (response.metadata.password !== providedPasswordHash) {
-            return res.status(401).json({ error: 'Incorrect password' });
-        }
-    }
 
-    return res.json(response);
-});
+        return res.json(output);
+    });
+
+    // 2. Fetch File Contents
+    app.get('/api/file', (req, res) => {
+        const dir = req.query.file_path?.split(/\/|\\/g).filter(Boolean) ?? [];
+
+        let response = root.root;
+
+        for (const d of dir) {
+            if (d === "root" || d === '' || d === 'sub_dir' || d === 'metadata') continue;
+
+            if (!response.sub_dir || !Object.hasOwn(response.sub_dir, d)) {
+                return res.status(404).json({ error: 'File not found' });
+            }
+            response = response.sub_dir[d];
+        }
+        if (!response || response.sub_dir !== undefined) {
+            return res.status(400).json({ error: 'Path points to a directory, not a file' });
+        }
+        if (response.metadata?.password !== undefined) {
+            const providedPasswordHash = hash(req.query.password ?? '');
+            if (response.metadata.password !== providedPasswordHash) {
+                return res.status(401).json({ error: 'Incorrect password' });
+            }
+        }
+
+        return res.json(response);
+    });
+
+    app.post('/api/register_group',(req,res)=>{
+            const {group_name} = req.body;
+            if(Object.hasOwn(points,group_name)){
+                return res.status(400).json({error:"Group name already exists!"});
+            }
+            points[group_name] = 0;
+            return res.json({message:"Group registered successfully!"});  
+    })
 }
 
 // Start the server
